d4dc8a45acc2b570bf2948189e93aae0b2b5d9e8
Arne Hilbig <666arnehilbig@gmail.com>
2023-11-27 12:24:35 +0100

    Added better test_videp_transmitter

--- vehicle_tracking_configurator/test_video_transmitter.py
+++ vehicle_tracking_configurator/test_video_transmitter.py
@@ -1,9 +1,25 @@
-import cv2
-import pynng
-import time
-import numpy as np
+# Copyright (C) 2023, NG:ITL
 
-source = cv2.VideoCapture("resources/test_video_1.h265")
+from pathlib import Path
+import numpy as np
+import time
+import sys
+
+import pynng
+import cv2
+
+def find_base_directory() -> Path:
+    """Find the base directory of the project."""
+    search_paths = {Path().cwd(), Path().cwd().parent, Path(__file__).parent.parent}
+    for directory in search_paths:
+        if (directory / "vehicle_tracking_configurator_config.json").exists():
+            return directory
+    sys.exit(1)
+
+base_dir = find_base_directory()
+
+source = cv2.VideoCapture(str(base_dir / "resources" / "test_video_1.h265"))
+
 pub = pynng.Pub0(listen="ipc:///tmp/RAAI/camera_frame.ipc")
 
 while True:
