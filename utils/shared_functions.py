4dfbbf6b25d55f8b9ff684b25e4d114f6658ab18
Arne Hilbig <666arnehilbig@gmail.com>
2023-12-21 12:52:52 +0100

    Updated file to use work with pyinstaller

--- utils/shared_functions.py
+++ utils/shared_functions.py
@@ -7,35 +7,20 @@ from json import load
 from typing import Any
 
 
-FILE_DIR = Path(__file__).parent
-BASE_DIR = FILE_DIR.parent
-SCHEMA_DIR = BASE_DIR / "vehicle_tracking_configurator" / "schema"
+MAIN_DIR = Path(__file__).parent.parent
 
 
 class DirectoryNotFoundError(Exception):
     """Raised when the directory is not found.
-    
+
     Args:
-        
+
     """
 
     def __init__(self, message: str = "directory was not found.") -> None:
         self.message = message
         super().__init__(self.message)
 
-
-def find_base_directory() -> tuple[Path, None | DirectoryNotFoundError]:
-    """Find the base directory of the project. If not found exits the program.
-
-    Returns:
-        Path: The base directory or any empty path.
-        None | DirectoryNotFoundError: None if the directory is found, else a directory not found error.
-    """
-    search_paths = {Path().cwd(), Path().cwd().parent, FILE_DIR.parent}
-    for directory in search_paths:
-        if (directory / "vehicle_tracking_configurator_config.json").exists():
-            return (directory, None)
-    return (Path(), DirectoryNotFoundError("Base directory was not found."))
 
 def get_all_schemas() -> dict[str, dict[str, Any]]:
     """Get all the schemas in the schemas directory.
@@ -44,7 +29,7 @@ def get_all_schemas() -> dict[str, dict[str, Any]]:
         list[Path]: A list of all the schemas.
     """
     schemas: dict[str, dict[str, Any]] = {}
-    for schema in SCHEMA_DIR.glob("*.json"):
+    for schema in (MAIN_DIR / "schema").glob("*.json"):
         with open(schema, "r", encoding="utf-8") as schema_file:
             schema_name = schema.stem
             schemas[schema_name] = load(schema_file)
