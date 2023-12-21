4e4495d58304a44e9280ffc32f748b362489bb4f
Arne Hilbig <666arnehilbig@gmail.com>
2023-12-21 12:54:40 +0100

    Changed functionality as this will not be called in the EXE

--- utils/test_video_transmitter.py
+++ utils/test_video_transmitter.py
@@ -1,20 +1,16 @@
-"""Transmits a video file over IPC to the RAAI camera frame subscriber."""""
+"""Transmits a video file over IPC to the RAAI camera frame subscriber.""" ""
 # Copyright (C) 2023, NG:ITL
 
+from pathlib import Path
 import time
 
 import numpy as np
 import pynng
 import cv2
 
-from utils.shared_functions import find_base_directory
 
+BASE_DIR = Path(__file__).parent.parent
 
-BASE_DIR, error = find_base_directory()
-
-if error:
-    raise error
-del error
 
 source = cv2.VideoCapture(str(BASE_DIR / "resources" / "test_video_1.h265"))
 
